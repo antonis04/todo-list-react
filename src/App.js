56f9790bf5c0587b7af667f9e9e155cb32242278
Antek <antoni.samol@gmail.com>
2025-04-10 14:55:09 +0200

    changed task content

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Container from "./Container";
 
 const tasks = [
   {id: 1, content: "przejsc na reacta", done: false },
-  {id: 2, content: "zjeść obiad", done: true },
+  {id: 2, content: "zjeść kolacje", done: true },
 ];
 
 const hideDoneTasks = true;
