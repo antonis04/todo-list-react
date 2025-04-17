0b939db601b8b571ec00a84102ebc0e3fc8f44d8
Antek <antoni.samol@gmail.com>
2025-04-17 16:08:59 +0200

    Lesson completed

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,17 +1,26 @@
 import "./style.css";
 
-const Tasks = ({ tasks }) => (
-  <ul className="tasks">
-    {tasks.map(({ done, content }) => (
-      <li className={`task-item${done ? " task-item--completed" : ""}`}>
-        <button className="js-done task-item__done">✔</button>
-        <span className="task-item__content">{content}</span>
-        <button className="js-remove task-item__remove">🗑</button>
-      </li>
-    ))}
-  </ul>
+const deleteTask = (name) => {
+  console.log(`Nazwa zadania do usunięcia: ${name}`);
+};
 
-
-);
+const Tasks = ({ tasks }) => {
+  return (
+    <ul className="tasks">
+      {tasks.map(({ done, content }) => (
+        <li className={`task-item${done ? " task-item--completed" : ""}`} key={content}>
+          <button className="js-done task-item__done">✔</button>
+          <span className="task-item__content">{content}</span>
+          <button
+            className="js-remove task-item__remove"
+            onClick={() => deleteTask(content)}
+          >
+            🗑
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default Tasks;
