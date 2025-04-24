1e43c5984b1b5e830c2fe2efd12e126d2676eade
Antek <antoni.samol@gmail.com>
2025-04-24 14:44:01 +0200

    toggleHideDone

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -18,9 +19,14 @@ const tasks = [
 ];
 
 
-const hideDone = false;
-
 function App() {
+  const [hideDone, setHideDone] = useState(false);
+
+const toggleHideDone = () => {
+    setHideDone(hideDone => !hideDone);
+  };
+
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -31,7 +37,11 @@ function App() {
         title="Lista zadań"
         body={<Tasks tasks={tasks} hideDone={hideDone} />}
         extraHeaderContent={
-          <Buttons tasks={tasks} hideDone={hideDone} />
+          <Buttons 
+          tasks={tasks} 
+          hideDone={hideDone}
+          toggleHideDone={toggleHideDone}
+          />
         }
       />
     </Container>
