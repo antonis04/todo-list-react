e2b5ca92e0549b1b50a750203a4cf9b0c830ced7
Antek <antoni.samol@gmail.com>
2025-05-17 11:45:44 +0200

    media updated

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -7,13 +7,23 @@ export const Forms = styled.form`
   padding: 20px;
   background-color: white;
   margin-bottom: 10px;
+
+  @media (max-width: 767px) {
+    flex-direction: column;
+    gap: 15px;
+  }
 `;
+
 export const FormInput = styled.input`
   flex: 1;
   padding: 10px;
   font-size: 16px;
   border: 1px solid #ccc;
   border-radius: 4px;
+
+  @media (max-width: 767px) {
+    width: 100%;
+  }
 `;
 
 export const FormButton = styled.button`
@@ -25,7 +35,12 @@ export const FormButton = styled.button`
   color: #fff;
   cursor: pointer;
   transition: background-color 0.3s;
+
   &:hover {
-      background-color: #008080;
+    background-color: #008080;
+  }
+
+  @media (max-width: 767px) {
+    width: 100%;
   }
 `;
